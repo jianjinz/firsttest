306442514d73ebbe3901f3ee8fe09ac287d9198a
jinze <jinze@DESKTOP-4KMK9GO>
2019-02-01 13:19:52 -0500

    hello

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,13 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
+ *@JinzeJiang 991 390 627
  * @author Ronak
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c = new Card();
     }
     
 }
